e143f7c71ec8c8c22ffee45dc9a4e8a80ddd4ba5
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-05 23:49:04 +0300

    feat:s3

---
+++ Project-02-2-tier-app/s3.tf
@@ -0,0 +1,21 @@
+resource "yandex_storage_bucket" "app" {
+  folder_id = var.folder_id
+  bucket = "app-proj2-m1xxos"
+}
+
+resource "yandex_iam_service_account" "s3-sa" {
+  name      = "s3-proj2-sa"
+}
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.s3-sa.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.s3-sa.id
+  description        = "static access key for object storage"
+}
