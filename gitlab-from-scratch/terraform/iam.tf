448be55752fbf75a2933515561d0325797c7f610
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-20 22:05:37 +0300

    feat:k8s node sa

--- gitlab-from-scratch/terraform/iam.tf
+++ gitlab-from-scratch/terraform/iam.tf
@@ -3,8 +3,17 @@ variable "sa-k8s" {
   default = "sa-k8s"
 }
 
+variable "sa-k8s-node" {
+  type = string
+  default = "sa-k8s-node"
+}
+
 resource "yandex_iam_service_account" "sa-k8s" {
     name = var.sa-k8s
+}
+
+resource "yandex_iam_service_account" "sa-k8s-node" {
+  name = var.sa-k8s-node
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "k8s-editor" {
@@ -13,5 +22,14 @@ resource "yandex_resourcemanager_folder_iam_binding" "k8s-editor" {
   
   members = [ 
     "serviceAccount:${yandex_iam_service_account.sa-k8s.id}"
+   ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "k8s-puller" {
+  folder_id = yandex_resourcemanager_folder.gitlab.id
+  role = "container-registry.images.puller"
+
+  members = [ 
+    "serviceAccount:${yandex_iam_service_account.sa-k8s-node.id}"
    ]
 }
