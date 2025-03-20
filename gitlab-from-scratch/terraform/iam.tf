e1020605e76774945706d137ba209ff0400a4860
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-20 21:47:04 +0300

    fix:best practices

--- gitlab-from-scratch/terraform/iam.tf
+++ gitlab-from-scratch/terraform/iam.tf
@@ -1,5 +1,10 @@
+variable "sa-k8s" {
+  type = string
+  default = "sa-k8s"
+}
+
 resource "yandex_iam_service_account" "sa-k8s" {
-    name = "sa-k8s"
+    name = var.sa-k8s
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "k8s-editor" {
