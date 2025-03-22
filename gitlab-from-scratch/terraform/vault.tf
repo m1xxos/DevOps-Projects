1556909ca62f7545ceaf8ec5c914f0fa9bca55d5
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-22 23:11:00 +0300

    fix:remove helm version

--- gitlab-from-scratch/terraform/vault.tf
+++ gitlab-from-scratch/terraform/vault.tf
@@ -39,30 +39,3 @@ resource "yandex_kubernetes_marketplace_helm_release" "vault" {
     "server.extraEnvironmentVars.YANDEXCLOUD_KMS_KEY_ID" = yandex_kms_symmetric_key.vault-key.id
   }
 }
-
-# resource "helm_release" "vault" {
-#   name = "vault"
-#   namespace = kubernetes_namespace.vault.metadata[0].name
-#   repository = "oci://cr.yandex/yc-marketplace/yandex-cloud/vault/chart/"
-#   chart = "vault"
-#   version = "0.28.1+yckms"
-#   wait = true
-#   set = [
-#       {
-#         name = "yandexKmsAuthJson"
-#         value = file("./authorized-key.json")
-#         # value = jsonencode({
-#         #   id = yandex_iam_service_account_key.vault-sa-key.id
-#         #   service_account_id = yandex_iam_service_account_key.vault-sa-key.service_account_id
-#         #   created_at = yandex_iam_service_account_key.vault-sa-key.created_at
-#         #   key_algorithm = yandex_iam_service_account_key.vault-sa-key.key_algorithm
-#         #   public_key = yandex_iam_service_account_key.vault-sa-key.public_key
-#         #   private_key = yandex_iam_service_account_key.vault-sa-key.private_key
-#         # })
-#       },
-#       {
-#         name = "yandexKmsKeyId"
-#         value = yandex_kms_symmetric_key.vault-key.id
-#       }
-#     ]
-# }
