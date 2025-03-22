17148d5f3150742480fd11c2a1ffbe668290b7af
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-22 22:01:26 +0300

    feat:vault sa

--- gitlab-from-scratch/terraform/iam.tf
+++ gitlab-from-scratch/terraform/iam.tf
@@ -8,12 +8,21 @@ variable "sa-k8s-node" {
   default = "sa-k8s-node"
 }
 
+variable "vault-kms" {
+  type    = string
+  default = "vault-kms"
+}
+
 resource "yandex_iam_service_account" "sa-k8s" {
   name = var.sa-k8s
 }
 
 resource "yandex_iam_service_account" "sa-k8s-node" {
   name = var.sa-k8s-node
+}
+
+resource "yandex_iam_service_account" "vault-kms" {
+  name = var.vault-kms
 }
 
 resource "yandex_resourcemanager_folder_iam_binding" "k8s-editor" {
@@ -32,4 +41,17 @@ resource "yandex_resourcemanager_folder_iam_binding" "k8s-puller" {
   members = [
     "serviceAccount:${yandex_iam_service_account.sa-k8s-node.id}"
   ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "name" {
+  folder_id = yandex_resourcemanager_folder.gitlab.id
+  role      = "kms.keys.encrypterDecrypter"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.vault-kms.id}"
+  ]
+}
+
+resource "yandex_iam_service_account_key" "vault-sa-key" {
+  service_account_id = yandex_iam_service_account.vault-kms
 }
