dee7596d8fbbd28ae7f2860507353496fc8a7865
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-22 22:47:40 +0300

    feat:helm release test

--- gitlab-from-scratch/terraform/vault.tf
+++ gitlab-from-scratch/terraform/vault.tf
@@ -5,6 +5,46 @@ resource "yandex_kms_symmetric_key" "vault-key" {
 }
 
 output "key" {
-  value = yandex_iam_service_account_key.vault-sa-key.public_key
+  value = jsonencode({
+          id = yandex_iam_service_account_key.vault-sa-key.id
+          service_account_id = yandex_iam_service_account_key.vault-sa-key.service_account_id
+          created_at = yandex_iam_service_account_key.vault-sa-key.created_at
+          key_algorithm = yandex_iam_service_account_key.vault-sa-key.key_algorithm
+          public_key = yandex_iam_service_account_key.vault-sa-key.public_key
+          private_key = yandex_iam_service_account_key.vault-sa-key.private_key
+        })
   sensitive = true
+}
+
+resource "kubernetes_namespace" "vault" {
+  metadata {
+    name = "vault"
+  }
+}
+
+resource "helm_release" "vault" {
+  name = "vault"
+  namespace = kubernetes_namespace.vault.metadata[0].name
+  repository = "oci://cr.yandex/yc-marketplace/yandex-cloud/vault/chart/"
+  chart = "vault"
+  version = "0.28.1+yckms"
+  wait = true
+  set = [
+      {
+        name = "yandexKmsAuthJson"
+        value = file("./authorized-key.json")
+        # value = jsonencode({
+        #   id = yandex_iam_service_account_key.vault-sa-key.id
+        #   service_account_id = yandex_iam_service_account_key.vault-sa-key.service_account_id
+        #   created_at = yandex_iam_service_account_key.vault-sa-key.created_at
+        #   key_algorithm = yandex_iam_service_account_key.vault-sa-key.key_algorithm
+        #   public_key = yandex_iam_service_account_key.vault-sa-key.public_key
+        #   private_key = yandex_iam_service_account_key.vault-sa-key.private_key
+        # })
+      },
+      {
+        name = "yandexKmsKeyId"
+        value = yandex_kms_symmetric_key.vault-key.id
+      }
+    ]
 }
