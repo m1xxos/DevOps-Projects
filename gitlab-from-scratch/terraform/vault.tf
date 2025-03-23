94ae928cfc8eac98de49e579e4dd74730e9c0b20
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-23 23:04:34 +0300

    fix:removed test var

--- gitlab-from-scratch/terraform/vault.tf
+++ gitlab-from-scratch/terraform/vault.tf
@@ -4,18 +4,6 @@ resource "yandex_kms_symmetric_key" "vault-key" {
   rotation_period   = "24h"
 }
 
-output "key" {
-  value = jsonencode({
-          id = yandex_iam_service_account_key.vault-sa-key.id
-          service_account_id = yandex_iam_service_account_key.vault-sa-key.service_account_id
-          created_at = yandex_iam_service_account_key.vault-sa-key.created_at
-          key_algorithm = yandex_iam_service_account_key.vault-sa-key.key_algorithm
-          public_key = yandex_iam_service_account_key.vault-sa-key.public_key
-          private_key = yandex_iam_service_account_key.vault-sa-key.private_key
-        })
-  sensitive = true
-}
-
 resource "kubernetes_namespace" "vault" {
   metadata {
     name = "vault"
@@ -26,7 +14,7 @@ resource "yandex_kubernetes_marketplace_helm_release" "vault" {
   name = "vault"
   namespace = "vault"
   cluster_id = yandex_kubernetes_cluster.gitlab-cluster.id
-  product_version = "f2ejsfsifanfq9h2ko3e"
+  product_version = "f2ejsfsifanfq9h2ko3e" // vault 0.29.0_yckms
   user_values = {
     yandexKmsAuthJson = jsonencode({
           id = yandex_iam_service_account_key.vault-sa-key.id
