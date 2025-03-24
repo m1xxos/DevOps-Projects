ec63edba4ece24d27fc8477ac19780e45fe14a9b
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-03-24 20:12:30 +0300

    feat:argocd chart

---
+++ gitlab-from-scratch/terraform/argo.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "7.8.13"
+  values     = [file("files/argo-values.yaml")]
+}
